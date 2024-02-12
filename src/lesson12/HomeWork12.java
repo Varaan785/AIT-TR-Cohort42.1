bdb9977ecfb9d8eeb06e45f2790043a5aadb1d6c
Varaan785 <156232089+Varaan785@users.noreply.github.com>
2024-02-12 17:07:09 +0100

    12.02.24

--- src/lesson12/HomeWork12.java
+++ src/lesson12/HomeWork12.java
@@ -1,5 +1,7 @@
 package lesson12;
 
+import java.util.Arrays;
+
 public class HomeWork12 {
     public static int
     lsearch(int[] array, int goal) {
@@ -12,6 +14,13 @@ public class HomeWork12 {
     }
 
     public static void main(String[] args) {
+        int[] array = {64, 25, 12, 22, 11};
+
+        System.out.println("Original array: " + Arrays.toString(array));
+
+        public static void selectionSort selectionSort(array);
+
+        System.out.println("Sorted array: " + Arrays.toString(array));
 
         // task #1
         System.out.println("task #1");
@@ -25,7 +34,25 @@ public class HomeWork12 {
         }
         // task #2
         System.out.println("task #2");
+        ( int[] array){
+            int n = array.length;
 
+            for (int i = 0; i < n - 1; i++) {
+                int minIndex = i;
+
+                for (int j = i + 1; j < n; j++) {
+                    if (array[j] < array[minIndex]) {
+                        minIndex = j;
+                    }
+                }
+
+                int temp = array[minIndex];
+                array[minIndex] = array[i];
+                array[i] = temp;
+
+
+            }
+        }
     }
 
 }
