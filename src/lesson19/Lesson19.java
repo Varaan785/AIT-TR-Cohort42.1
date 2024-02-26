019366c1603e9e1be1d0bd1deb552a6480bad168
Varaan785 <156232089+Varaan785@users.noreply.github.com>
2024-02-26 23:49:00 +0100

    ,

--- src/lesson19/Lesson19.java
+++ src/lesson19/Lesson19.java
@@ -5,6 +5,12 @@ public class Lesson19 {
         RubberArray ra = new RubberArray();
         ra.add(8);
         ra.add(12);
+        ra.add(-1);
+        ra.add(3);
+        ra.add(25);
+        System.out.println(ra);
+        System.out.println(ra.get(2));
+        ra.remove(2);
         System.out.println(ra);
     }
 }
