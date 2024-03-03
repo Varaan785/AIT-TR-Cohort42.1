fa16c430c647be4268d6d5d7fd4f7e52f19baace
Varaan785 <156232089+Varaan785@users.noreply.github.com>
2024-03-03 20:00:54 +0100

    .

--- src/lesson19/homeWork19/HomeWork19.java
+++ src/lesson19/homeWork19/HomeWork19.java
@@ -1,4 +1,25 @@
-package lesson19.homeWork19;
+package lesson19.homeWork19;/**
+ * AIT-TR, Cohoirt 42.1, Java Basic, HW #19
+ *
+ * @author Valerian
+ * @version 28-Feb-34
+ */
 
 public class HomeWork19 {
+    public static void main(String[] args) {
+
+        //task #1
+        System.out.println("task #1");
+        RubberArrayHW19 rb = new RubberArrayHW19();
+        rb.add(8);
+        rb.add(4);
+        rb.add(-3);
+        rb.add(12);
+        rb.add(1);
+        System.out.println(rb);
+        //task #2
+        System.out.println("task #2");
+        System.out.println(rb.contains(12));
+        System.out.println(rb.indexOf(5));
+    }
 }
--- src/lesson19/homeWork19/RubberArrayHW19.java
+++ src/lesson19/homeWork19/RubberArrayHW19.java
@@ -1,8 +1,14 @@
 package lesson19.homeWork19;
+/**
+ * AIT-TR, Cohoirt 42.1, Java Basic, HW #19
+ * RubberArray
+ * @author Valerian
+ * @version 28-Feb-34
+ */
 
 public class RubberArrayHW19 {
     private final int INIT_DATA_SIZE = 10;
-    private final float RESIZE_KOEF = 1.5f;
+    private final float EXTEND_INDEX = 1.5f;
     private int[] data;
     private int length;
 
@@ -10,14 +16,11 @@ public class RubberArrayHW19 {
         data = new int[INIT_DATA_SIZE];
         length = 0;
     }
-    public int get(int idx) {
-        return data[idx];
-    }
-
-    public void add(int value) {
+    public int get(int idx) {return data[idx];}
+    private void extendArrayIfNeed() {
         if (length < data.length) {
-            // create new array, length *= 1.5
-            int[] newData = new int[(int) (data.length * RESIZE_KOEF)];
+            // create new array, length * EXTEND_INDEX
+            int[] newData = new int[(int) (data.length * EXTEND_INDEX)];
             // move all items to new array
             for (int i = 0; i < data.length; i++) {
                 newData[i] = data[i];
@@ -25,12 +28,24 @@ public class RubberArrayHW19 {
             // change link to new array
             data = newData;
         }
+    }
+    public void add(int value) {
+        extendArrayIfNeed();
         // add value
         data[length] = value;
         length++;
     }
     public void add(int value, int idx) {
-        //TODO implement
+        extendArrayIfNeed();
+        // insert value
+        // 0;1;2;3;4;5
+        // 8,4,3,2,1
+        for (int i = length; i > idx; i--) {
+            data[i] = data[i -1];
+
+        }
+        data[idx] = value;
+        length++;
     }
 
     public void remove(int idx) {
@@ -40,18 +55,29 @@ public class RubberArrayHW19 {
         }
         length--;
     }
+    public boolean contains(int value) {
+        return indexOf(value) != -1;
+    }
 
+    public int indexOf(int value) {
+        for (int i = 0; i < length; i++) {
+            if (data[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
     @Override
     public String toString () {
         //TODO use StringBuilder instead of String
-        String str = "[";
+        StringBuilder sb = new StringBuilder("[") ;
         for (int i = 0; i < length; i++) {
-            str += String.valueOf(data[i]);
+            sb.append(data[i]);
             if (i < length - 1) {
-                str += ", ";
+                sb.append(", ");
             }
         }
-        return str + "]";
+        return sb.append( "]").toString();
     }
 }
 
