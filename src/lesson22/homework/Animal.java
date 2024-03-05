b39f41916e9fbf4444e3386b21e85306b19e1f80
Varaan785 <156232089+Varaan785@users.noreply.github.com>
2024-03-05 20:50:10 +0100

    ,

---
+++ src/lesson22/homework/Animal.java
@@ -0,0 +1,10 @@
+package lesson22.homework;
+
+public class Animal {
+    String name;
+    int size;
+    public void name() {
+
+
+    }
+}
---
+++ src/lesson22/homework/Cathw22.java
@@ -0,0 +1,6 @@
+package lesson22.homework;
+
+public class Cathw22 extends Animal{
+    public String
+
+}
