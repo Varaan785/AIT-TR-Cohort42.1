606647016a63fcd86617f284f6f634a306e260d4
Varaan785 <156232089+Varaan785@users.noreply.github.com>
2024-03-22 22:39:52 +0100

    v001

--- src/lesson22/Rectangel.java
+++
@@ -1,7 +0,0 @@
-package lesson22;
-
-public class Rectangel extends Shape2D{
-    public Rectangel(int a, int b) {
-        super(a, b);
-    }
-}
---
+++ src/lesson22/RectangelL22.java
@@ -0,0 +1,13 @@
+package lesson22;
+/**
+ * AIT-TR, Cohort 42.1, Java Basic, Lesson #22
+ * RectangleL22
+ * @author Valerian
+ * @version 4-Mar-34
+ */
+
+public class RectangelL22 extends Shape2DL22 {
+    public RectangelL22(int a, int b) {
+        super(a, b);
+    }
+}
